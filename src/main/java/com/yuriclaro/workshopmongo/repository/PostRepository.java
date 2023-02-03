9699a6c6391513fbc12b5e802bdf16a993083c6b
Yuri Claro <yuri.claro@outlook.com>
2023-02-02 21:56:17 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/yuriclaro/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.yuriclaro.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yuriclaro.workshopmongo.domain.Post;
+import com.yuriclaro.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+}
