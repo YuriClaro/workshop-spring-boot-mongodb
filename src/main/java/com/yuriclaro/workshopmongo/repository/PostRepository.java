4eddbe4744feed86b40abe375858f1e8d464c0a6
Yuri Claro <yuri.claro@outlook.com>
2023-02-07 22:40:23 -0300

    Consulta simples com query methods

--- src/main/java/com/yuriclaro/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/yuriclaro/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package com.yuriclaro.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yuriclaro.workshopmongo.domain.Post;
-import com.yuriclaro.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
